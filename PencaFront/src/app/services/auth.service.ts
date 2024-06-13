c8f1fd89e18956287ae58981248f08573467e1b9
sofia <sofihernande123@gmail.com>
2024-06-13 12:03:49 -0300

    hecho

--- PencaFront/src/app/services/auth.service.ts
+++ PencaFront/src/app/services/auth.service.ts
@@ -3,6 +3,8 @@ import { AlumnoService } from './alumno.service';
 import { AdministradorService } from './administrador.service';
 import { Router } from '@angular/router';
 import { Alumno } from '../interfaces/alumnoInterface';
+import { CarreraService } from './carrera.service';
+import { Carrera } from '../interfaces/carrera';
 
 @Injectable({
   providedIn: 'root'
@@ -11,23 +13,22 @@ export class AuthService {
   private adminCredentials = { cedula: 999, contrasena: 'admin' };
   private usuarioAutenticado: Alumno | null = null;
 
-  constructor(private alumnoService: AlumnoService, private administradorService: AdministradorService, private router: Router) { }
+  constructor(
+    private alumnoService: AlumnoService, 
+    private administradorService: AdministradorService, 
+    private carreraService: CarreraService,
+    private router: Router
+  ) {}
 
   autenticarUsuario(cedula: number, contrasena: string): { esAdmin: boolean, usuario?: any } | null {
-    // Verificar si las credenciales son del administrador
     if (cedula === this.adminCredentials.cedula && contrasena === this.adminCredentials.contrasena) {
-      // Si son del administrador, devolver la información de administrador
       return { esAdmin: true, usuario: this.administradorService.obtenerDatosAdmin() };
     } else {
-      // Si no son del administrador, buscar en los alumnos
       const usuario = this.alumnoService.obtenerUsuarioPorCedulaYContrasena(cedula, contrasena);
       if (usuario) {
-        // Si se encuentra al alumno, actualizar el usuario autenticado
         this.usuarioAutenticado = usuario;
-        // Devolver la información del alumno
         return { esAdmin: false, usuario };
       } else {
-        // Si no se encuentra al alumno, devolver null
         return null;
       }
     }
@@ -42,7 +43,6 @@ export class AuthService {
 
   logout(): void {
     this.usuarioAutenticado = null;
-  
     this.router.navigate(['/login']);
   }
 }

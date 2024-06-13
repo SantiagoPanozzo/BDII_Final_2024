b997672d0f6b11383974e369081ca9686f49dc25
SantiagoPanozzo <97817883+SantiagoPanozzo@users.noreply.github.com>
2024-06-13 12:35:24 -0300

    Datos de prueba para carrera

--- PencaAPI/PencaDB/002_datos_de_prueba.sql
+++ PencaAPI/PencaDB/002_datos_de_prueba.sql
@@ -2,3 +2,4 @@ INSERT INTO equipo (abreviatura, pais) VALUES ('uyu', 'Uruguay');
 INSERT INTO equipo (abreviatura, pais) VALUES ('arg', 'Argentina');
 INSERT INTO Alumno (nombre, apellido, cedula, fecha_nacimiento, anio_ingreso, semestre_ingreso, puntaje_total, campeon, subcampeon) VALUES ('nombre', 'apellido', 123456789, '2021-01-01', 2021, 1, 0, 'uyu', 'arg');
 INSERT INTO Alumno (nombre, apellido, cedula, fecha_nacimiento, anio_ingreso, semestre_ingreso, puntaje_total, campeon, subcampeon) VALUES ('nombre2', 'apellido2', 987654321, '2022-02-01', 2022, 2, 0, 'uyu', 'arg');
+INSERT INTO carrera(nombre) VALUES ('Ingeniería en Informática');
